69f081c18a46b5b0a0243a7af4836baf13020962
Dan Tormin <mintz.dnt@gmail.com>
2025-03-07 16:04:02 +0500

    add nginx for fast deploy

--- install.sh
+++ install.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-read -p "The full path to build the project (Press [ENTER] for default: /var/www/athena): " BUILD_OUTPUT_PATH
-BUILD_OUTPUT_PATH=${BUILD_OUTPUT_PATH:-/var/www/athena}
+export $(cat .env | xargs)
 
-read -p "Backend URL with protocol (for example, http://192.168.100.250/api/): " BACKEND_API_URL
-if [[ -z "$BACKEND_API_URL" ]]; then
-    echo "Error: Backend URL cannot be empty!"
-    exit 1
-fi
 
-echo "VITE_BUILD_OUTPUT_PATH=$BUILD_OUTPUT_PATH" > .env
-echo "VITE_BACKEND_API_URL=\"$BACKEND_API_URL\"" >> .env
-
-echo "Environment variables saved to .env:"
-cat .env
-
-sudo mkdir -p $BUILD_OUTPUT_PATH
+sudo mkdir -p $VITE_BUILD_OUTPUT_PATH
 npm i
 npm run build
+
+project_path='pwd'
+
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s $project_path/nginx/site.conf /etc/nginx/sites-enabled/default
+sudo service nginx restart
